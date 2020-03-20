f6c7c4141d0b134b8121e1b0b246afe0776e19a6
ivan-boginski <aisivan36@gmail.com>
2020-03-20 17:07:33 +0700

    added a new bug to fix it later

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 
 class NewsList extends StatelessWidget {
@@ -7,33 +6,7 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: buildList(),
-    );
-  }
-
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, int index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-            return Container(
-              height: 80.0,
-              child: snapshot.hasData
-                  ? Text('Im visible $index')
-                  : Text('I havent fetched data yet $index'),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  getFuture() {
-    return Future.delayed(
-      Duration(seconds: 2),
-      () => 'hi',
+      body: Text('Show a list'),
     );
   }
 }
