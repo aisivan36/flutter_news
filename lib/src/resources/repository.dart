27b3f39e1178037ed262dcf9d924ffd035e5140e
ivan-boginski <aisivan36@gmail.com>
2020-03-20 15:16:16 +0700

    git push

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'news_api_provider.dart';
+import 'news_db_provider.dart';
+import '../models/item_model.dart';
+
+class Repository {
+  List<Source> sources = <Source>[
+    NewsDbProvider(),
+    NewsApiProvider(),
+  ];
+  List<Cache> caches = <Cache>[
+    NewsDbProvider(),
+  ];
+
+  Future<List<int>> fetchTopIds() {
+    return apiProvider.fetchTopIds();
+  }
+
+  Future<ItemModel> fetchItem(int id) async {
+    var item = await dbProvider.fetchItem(id);
+    if (item != null) {
+      return item;
+    }
+
+    item = await apiProvider.fetchItem(id);
+    dbProvider.addItem(item);
+
+    return item;
+  }
+}
+
+abstract class Source {
+  Future<List<int>> fetchTopIds();
+  Future<ItemModel> fetchItem(int id);
+}
+
+abstract class Cache {
+  Future<int> addItem(ItemModel item);
+}
