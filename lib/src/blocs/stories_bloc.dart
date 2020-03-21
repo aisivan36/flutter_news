1c1fa5aec4f555a8d4c1da5d595c975df2d899a8
ivan-boginski <aisivan36@gmail.com>
2020-03-21 07:29:14 +0700

    Added a new bug to fix it later

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -3,5 +3,18 @@ import '../models/item_model.dart';
 import '../resources/repository.dart';
 
 class StoriesBloc {
-  
+  final _repository = Repository();
+  final _topIds = PublishSubject<List<int>>();
+
+  // Getters tp get a Streams
+  Stream<List<int>> get topIds => _topIds.stream;
+
+  fetchTopIds() async {
+    final ids = await _repository.fetchTopIds();
+    _topIds.sink.add(ids);
+  }
+
+  dispose() {
+    _topIds.close();
+  }
 }
